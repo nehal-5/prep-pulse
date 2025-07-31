bd3cf02a016d3c0487a49b3da1483779592f0c70
nehal-5 <nehalfathema08@gmail.com>
2025-07-31 17:35:09 +0530

    Small typo error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
-import Signup from './pages/SignUp';
+import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 import HomePage from './pages/HomePage';
